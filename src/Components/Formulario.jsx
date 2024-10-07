2f794ae46232b2c2c6a05d80899f4ed7d6c168b7
sebastiannumero <sebastian.ruiz78@uabc.edu.mx>
2024-10-07 14:35:00 -0700

    cambio subtitulo

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -91,7 +91,7 @@ const Formulario = ({ pacientes, setPacientes, setPaciente, paciente }) => {
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="text-lg mt-5 text-center">
-        Añade Pacientes y {""}
+        Añade a los Pacientes y {""}
         <span className="text-indigo-600 font-bold">Admistrarlos</span>
       </p>
       <form
